b2a159bb55a907927c60800db43c910cedd31552
Daniel Bobadilla <DanielBobadilla1995@gmail.com>
2017-09-15 13:53:03 -0400

    added a second js file, added donate image, created form tables for donate and question page

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
-var donationsForm, first_name, last_name, address, phone, 
-email, donation, formOfPayment;
+var  message, donationsForm, first_name, last_name, address, phone, 
+email, donation, formOfPayment, cardInfo, date;
 
 document.addEventListener("DOMContentLoaded",function(){
-	donationsForm = getElementByID
+	donationsForm = document.getElementById("donationsForm");
+	message = document.getElementById("message");
+	first_name = document.getElementById("first_name");
+	last_name = document.getElementById("last_name");
+	address = document.getElementById("address");
+	phone = document.getElementById ("phone");
+	email = document.getElementById("email");
+	donation = document.getElementById("donation");
+	formOfPayment = document.getElementById("formOfPayment");
+	cardInfo = document.getElementById("cardInfo");
+	date = document.getElementById("date")
+	donationsForm.addEventListener("submit",function(event){
+		console.log(event);
+		event.preventDefault();
+		message.innerText = "Thank you! A donation has been made in your name to the Open Arms Foundation! An email will be sent to you shortly regarding your donation.";
+		event.target.reset();
+		// clearForm(document.forms[0]);
+	});
 });
+
+// function clearForm(form){
+// 	form.childNodes.forEach(function(element){
+// 		if(element.tagName == "INPUT") {
+// 			element.style.display = "none";
+// 		}
+// 	});
+// }
